55a2d6320da259ae72f70c064e3dc8c3f58d0f70
Monika Chaudhary <chaudharymonika8630@gmail.com>
2025-06-26 17:33:23 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountid = 14553
+let accountEmail ="hitesh@gmail.com"
+var accountPasword = "12345"
+accountcity = "jaipur"
+let accountstate;
+
+// accountid = 2 is not allowed//
+
+accountEmail ="google@gmail.com"
+accountPasword = "123458"
+accountcity = "delhi"
+
+console.log(accountid);
+console.table([accountid,accountEmail,accountPasword,accountcity,accountstate]);
