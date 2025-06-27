7a83127da377709a25fd8fbe70d7bb453ab9de5f
Monika Chaudhary <chaudharymonika8630@gmail.com>
2025-06-27 17:05:38 +0000

    change config done

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,10 @@
+let score = 33 //33abc//null//undefined//boolean
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber = number(score)
+console.log(typeof  valueinnumber);
+console.log( valueinnumber);
+// "33" =>33
+    // "33abc"=> nan
+    //true =>1
