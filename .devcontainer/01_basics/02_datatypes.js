6fcaaad5e8ff18b301cac28d1f5c4f6a7f3e8f4f
Monika Chaudhary <chaudharymonika8630@gmail.com>
2025-06-27 16:39:18 +0000

    change config done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all code as new vwersion of js//
+
+//alert(3+3)//we are using nodejs not browser//
+
+console.log("hitseh"); console.log("me")
+
+let name = "hitesh"
+let age = 6
+let isloggedin = false
+
+//number=> 2 to power 53
+//bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+//symbol => unique//
+
+console.log(typeof "i");
+console.log(typeof null);//object 
+console.log(typeof undefined);//undefined
